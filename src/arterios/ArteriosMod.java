8ffb8ca5d213a5b5d8db6f6ebc6970b3eb771a90
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-07 11:56:29 +0200

    Update and rename ExampleJavaMod.java to ArteriosMod.java

---
+++ src/arterios/ArteriosMod.java
@@ -0,0 +1,23 @@
+package arterios;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class ArteriosMod extends Mod{
+
+    public ArteriosMod(){
+        Log.info("Loaded ArteriosMod constructor.");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading Arterios content.");
+    }
+
+}
