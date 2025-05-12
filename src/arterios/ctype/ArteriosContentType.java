8f9153181737c382441201cda35b575477e37972
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-12 07:50:13 +0200

    Update ArteriosContentType.java

--- src/arterios/ctype/ArteriosContentType.java
+++ src/arterios/ctype/ArteriosContentType.java
@@ -1,4 +1,4 @@
-package arterios.ctype;
+/*package arterios.ctype;
 
 import arc.util.*;
 import mindustry.ai.*;
@@ -7,7 +7,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 
 /** Do not rearrange, ever! */
-public enum ArteriosContentType{
+/** public enum ArteriosContentType{
     plasma(Plasma.class),
 
     public static final ArteriosContentType[] all = values();
@@ -17,4 +17,4 @@ public enum ArteriosContentType{
     ArteriosContentType(Class<? extends Content> contentClass){
         this.contentClass = contentClass;
     }
-}
+} */
