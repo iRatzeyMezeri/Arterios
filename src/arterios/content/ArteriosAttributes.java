b8d936f5528fe8682f5a65f27508c52266b20885
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-24 13:25:57 +0200

    Update ArteriosAttributes.java

--- src/arterios/content/ArteriosAttributes.java
+++ src/arterios/content/ArteriosAttributes.java
@@ -1,12 +1,18 @@
 package arterios.content;
 
-import mindustry.content.Blocks;
 import mindustry.world.meta.*;
+import arterios.content.ArteriosBlocks;
 
 public class ArteriosAttributes {
     public static void load(){
         Attribute.add("geocharge");
     }
 
-    public static void setAttributes(){}
+    public static void setAttributes(){
+        ArteriosBlocks.geochargedObsidian.attributes.set(Attribute.get("geocharge"), 1);
+        ArteriosBlocks.geochargedTachylite.attributes.set(Attribute.get("geocharge"), 1);
+        ArteriosBlocks.geochargedSodalite.attributes.set(Attribute.get("geocharge"), 1);
+        ArteriosBlocks.geochargedCassiterite.attributes.set(Attribute.get("geocharge"), 1);
+        ArteriosBlocks.geochargedPurpurite.attributes.set(Attribute.get("geocharge"), 1);
+    }
 }
