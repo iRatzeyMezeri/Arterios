526c26c3cad0062c6d4e78dda048728493a59e38
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-05-24 10:55:39 +0200

    Create ArteriosAttributes.java

---
+++ src/arterios/content/ArteriosAttributes.java
@@ -0,0 +1,10 @@
+package arterios.content;
+
+import mindustry.content.Blocks;
+import mindustry.world.meta.*;
+
+public class ArteriosAttributes {
+    public static void load(){
+        Attribute.add("geocharge");
+    }
+}
