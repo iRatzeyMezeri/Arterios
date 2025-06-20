a88100c6cde840ecc466338d3be08f677462cdc9
Andromeda-Galaxy29 <137232272+Andromeda-Galaxy29@users.noreply.github.com>
2025-06-20 17:38:13 +0300

    Cambru

--- src/arterios/content/ArteriosUnits.java
+++ src/arterios/content/ArteriosUnits.java
@@ -6,6 +6,12 @@ import arterios.entities.bullet.*;
 import arterios.entities.part.*;
 import mindustry.ai.types.GroundAI;
 import mindustry.content.Fx;
+import mindustry.entities.abilities.MoveEffectAbility;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.LaserBulletType;
+import mindustry.entities.part.HoverPart;
+import mindustry.entities.pattern.ShootHelix;
+import mindustry.gen.ElevationMoveUnit;
 import mindustry.gen.Sounds;
 import mindustry.gen.UnitWaterMove;
 import mindustry.type.UnitType;
@@ -19,6 +25,92 @@ public class ArteriosUnits {
     felix, lynx, silvester, leptail, namafelis;
 
     public static void load() {
+        cambru = new UnitType("cambru"){{
+            constructor = ElevationMoveUnit::create;
+
+            drag = 0.07f;
+            speed = 2f;
+            accel = 0.09f;
+
+            hitSize = 9;
+            rotateSpeed = 8;
+            health = 280;
+            armor = 0;
+            faceTarget = true;
+            targetAir = true;
+            outlineColor = APal.lightOutline;
+
+            hovering = true;
+
+            shadowElevation = 0.1f;
+
+            parts.addAll(
+                    new HoverPart(){{
+                        x = -1f;
+                        y = -4;
+                        mirror = false;
+                        radius = 5;
+                        phase = 90;
+                        stroke = 2;
+                        layerOffset = -0.01f;
+                        color = APal.hoverRed;
+                    }},
+                    new HoverPart(){{
+                        x = 3f - 1f;
+                        y = 3f;
+                        mirror = false;
+                        radius = 5;
+                        phase = 90;
+                        stroke = 2;
+                        layerOffset = -0.01f;
+                        color = APal.hoverRed;
+                    }},
+                    new HoverPart(){{
+                        x = -3f - 1f;
+                        y = 3f;
+                        mirror = false;
+                        radius = 5;
+                        phase = 90;
+                        stroke = 2;
+                        layerOffset = -0.01f;
+                        color = APal.hoverRed;
+                    }},
+                    new SillyPart("-bow")
+            );
+
+            abilities.add(new MoveEffectAbility(-1f, -6f, APal.hoverRed, Fx.missileTrailShort, 4f));
+
+            weapons.addAll(
+                    new Weapon("arterios-cambru-weapon"){{
+                        x = 4f;
+                        y = 0;
+                        reload = 48;
+                        shootSound = Sounds.lasershoot;
+                        mirror = false;
+                        rotate = true;
+                        rotationLimit = 25;
+                        bullet = new LaserBulletType(){{
+                            damage = 14;
+                            colors = new Color[]{
+                                    APal.hoverRed,
+                                    APal.hoverRed,
+                                    Color.white
+                            };
+                            hitEffect = Fx.hitLancer;
+                            sideLength = 2;
+                            hitSize = 3;
+                            lifetime = 8;
+                            drawSize = 300;
+                            collidesAir = true;
+                            length = 40;
+                            width = 8;
+                            pierce = false;
+                            pierceCap = 1;
+                        }};
+                    }}
+            );
+        }};
+
         felix = new UnitType("felix"){{
             constructor = UnitWaterMove::create;
             aiController = GroundAI::new;
